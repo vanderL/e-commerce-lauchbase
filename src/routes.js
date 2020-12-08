1ad01aaf0b0c9ca2cfb347df15c2a89281dbf221
vanderL <vnd.vander@gmail.com>
2020-12-07 22:36:29 -0300

    Structure form create products

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const ProductController = require('./app/controller/ProductController')
 
 
 router.get('/', function (req, res) {
     return res.render('layout.njk')
 })
 
+router.get('/products/create', ProductController.create)
+
+router.get('/ads/create', function(req, res) {
+    return res.redirect("/products/create")
+})
 
 
 module.exports = router
