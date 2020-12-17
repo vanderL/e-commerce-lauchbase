276bc20c91e76e6f05ea148025d472417cfb6f34
vanderL <vnd.vander@gmail.com>
2020-12-16 22:29:41 -0300

    Refactoring and Function for Frontend remove photo

--- public/script/main.js
+++ public/script/main.js
@@ -15,16 +15,12 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
     handleFileInput(event) {
         const { files: fileList } = event.target
-        const { uploadLimit } = PhotosUpload
 
-        if(fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} Fotos`)
-            event.preventDefault()
-            return
-        }
+        if(PhotosUpload.hasLimit(event, fileList)) return
         
         Array.from(fileList).forEach(file => {
             const reader =  new FileReader()
@@ -33,17 +29,47 @@ const PhotosUpload = {
                 const image = new Image()
                 image.src = String(reader.result)
 
-                const div = document.createElement('div')
-                div.classList.add('photo')
+                const div = PhotosUpload.getContainer(image)
 
-                div.onclick = () => alert('Remove photo')
-
-                div.appendChild(image)
-
-                document.querySelector('#photos-preview').appendChild(div)
+                PhotosUpload.preview.appendChild(div)
             }
 
             reader.readAsDataURL(file)
         })
+    },
+    hasLimit(event, fileList) {
+        const { uploadLimit } = PhotosUpload
+
+        if(fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} Fotos`)
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick = PhotosUpload.removePhoto
+
+        div.appendChild(image)
+        div.appendChild(PhotosUpload.getRemoveButton())
+
+        return div
+    },
+    getRemoveButton() {
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = 'close'
+        return button
+    },
+    removePhoto(event) {
+        const photoDiv = event.target.parentNode
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index = photosArray.indexOf(photoDiv)
+
+        photoDiv.remove()
     }
 }
