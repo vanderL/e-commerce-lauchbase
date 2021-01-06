b3589c4174e9fc4b8b5903fd60b527f2b632f4f1
vanderL <vnd.vander@gmail.com>
2021-01-05 22:02:31 -0300

    Config upload with multer

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controller/ProductController')
 
 
@@ -9,8 +10,8 @@ router.get('/', function (req, res) {
 
 router.get('/products/create', ProductController.create)
 router.get('/products/:id/edit', ProductController.edit)
-router.put('/products', ProductController.put)
-router.post('/products', ProductController.post)
+router.put('/products', multer.array("photos", 6),ProductController.put)
+router.post('/products', multer.array("photos", 6),ProductController.post)
 
 //Alias
 router.get('/ads/create', function(req, res) {
