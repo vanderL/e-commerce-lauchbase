a87f72723f7b8c226751cebcacd8b6cf8dd4c41a
vanderL <vnd.vander@gmail.com>
2021-01-13 22:17:53 -0300

    Footer fixed, page show with dinamyc data

--- src/app/controller/ProductController.js
+++ src/app/controller/ProductController.js
@@ -1,4 +1,4 @@
-const { formatPrice } = require('../../lib/utils')
+const { formatPrice, date } = require('../../lib/utils')
 
 const Category = require('../models/Category');
 const Product = require('../models/Product');
@@ -40,7 +40,22 @@ module.exports = {
 
     },
     async show(req, res) {
-        return res.render("products/show")
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if(!product) return res.send("Product Not Found!")
+
+        const {day, hour, minutes, month} = date(product.updated_at)
+
+        product.published = {
+            day: `${day}/${month}`,
+            hour: `${hour}h${minutes}`
+        }
+
+        product.oldPrice = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+
+        return res.render("products/show", { product })
     },
     async edit (req, res){
         let result = await Product.find(req.params.id)
@@ -102,7 +117,7 @@ module.exports = {
 
         await Product.update(req.body)
 
-        return res.redirect(`/products/${req.body.id}/edit`)
+        return res.redirect(`/products/${req.body.id}`)
     },
     async delete(req, res) {
         await Product.delete(req.body.id)
