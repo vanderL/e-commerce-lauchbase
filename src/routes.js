c989e50f32dbca14909e5508223d5547c58ab7e4
Vander Lima <vnd.vander@gmail.com>
2021-01-15 17:53:06 -0300

    init Structure PageSearch

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ const multer = require('./app/middlewares/multer')
 
 const ProductController = require('./app/controller/ProductController')
 const HomeController = require('./app/controller/HomeController')
-const Product = require('./app/models/Product')
+const SearchController = require('./app/controller/SearchController')
 
-
+//HOME
 router.get('/', HomeController.index)
 
+
+//PRODUCT
 router.get('/products/create', ProductController.create)
 router.get('/products/:id', ProductController.show) // arrumar! 
 router.get('/products/:id/edit', ProductController.edit)
@@ -17,10 +19,15 @@ router.post('/products', multer.array("photo", 6),ProductController.post)
 router.put('/products', multer.array("photo", 6),ProductController.put)
 router.delete('/products', ProductController.delete)
 
+//SEARCH
+router.get('/search/products', SearchController.index)
+
 //Alias
 router.get('/ads/create', function(req, res) {
     return res.redirect("/products/create")
 })
+
+
 
 
 module.exports = router
