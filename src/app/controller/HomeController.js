34d60be02e082760ace30546588b3351f484c8c7
Vander Lima <vnd.vander@gmail.com>
2021-01-15 16:55:37 -0300

    Homepage Styled -- Warning bug image products

--- src/app/controller/HomeController.js
+++ src/app/controller/HomeController.js
@@ -14,6 +14,8 @@ module.exports = {
             let results = await Product.files(productId)
             const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
         
+            console.log("Files!!!:    " + files)
+
             return files[0]
         }
 
@@ -22,8 +24,11 @@ module.exports = {
             product.oldPrice = formatPrice(product.old_price)
             product.price = formatPrice(product.price)
 
+            console.log("Produc.IMG!!!!!:    " + product.img)
+
             return product
         }).filter((product, index) => index > 2 ? false : true)
+
 
         const lastAdded = await Promise.all(productsPromise)
         return res.render("home/index", { products: lastAdded })
