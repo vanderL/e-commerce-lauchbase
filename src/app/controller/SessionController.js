adaba5dd99c4f2d16d706f470bd757d7f9b9b8cc
Vander Lima <vnd.vander@gmail.com>
2021-01-31 03:36:12 -0300

    Init forgot passoword

--- src/app/controller/SessionController.js
+++ src/app/controller/SessionController.js
@@ -10,5 +10,17 @@ module.exports = {
     logout(req, res) {
         req.session.destroy()
         return res.redirect('/')
+    },
+    forgotForm(req, res) {
+        return res.render('session/forgot-password')
+    },
+    forgot(req, res) {
+        // um token para esse usuário 
+
+        // criar uma expiração do token
+
+        // enviar um email com um link de recuperação
+
+        //avisar o usuário que enviamos o link
     }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -12,12 +12,12 @@ const { isLoggedRedirectToUsers, onlyUsers } = require('../app/middlewares/sessi
 router.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
 router.post('/login',SessionValidator.login, SessionController.login)
 router.post('/logout', SessionController.logout)
-/*
+
 //reset password / forgot
 router.get('/forgot-password', SessionController.forgotForm)
-router.get('/password-reset', SessionController.resetForm)
-router.post('/forgot-password', SessionController.forgot)
-router.post('/reset-password', SessionController.reset)
+//router.get('/password-reset', SessionController.resetForm)
+router.post('/forgot-password',SessionValidator.forgot, SessionController.forgot)
+//router.post('/reset-password', SessionController.reset)
 
 //user register UserController */
 router.get('/register', UserController.registerForm)
