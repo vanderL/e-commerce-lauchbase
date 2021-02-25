bc5112212191311502804eadbcd489e84f551e46
Vander Lima <vnd.vander@gmail.com>
2021-02-25 16:29:03 -0300

    Using the Service ProductLoad

--- src/app/controller/ProductController.js
+++ src/app/controller/ProductController.js
@@ -3,8 +3,7 @@ const { unlinkSync } = require('fs')
 const Category = require('../models/Category');
 const Product = require('../models/Product');
 const File = require('../models/File');
-
-const { formatPrice, date } = require('../../lib/utils')
+const LoadProductsService =  require('../services/LoadProductsService')
 
 module.exports = {
     
@@ -57,27 +56,13 @@ module.exports = {
     },
     async show(req, res) {
         try {
-            const product = await Product.find(req.params.id)
+            const product = await LoadProductsService.load('product', {
+                where: {
+                    id: req.params.id
+                }
+            })
             
-            if(!product) return res.send("Product Not Found!")
-
-            const {day, hour, minutes, month} = date(product.updated_at)
-
-            product.published = {
-                day: `${day}/${month}`,
-                hour: `${hour}h${minutes}`
-            }
-
-            product.oldPrice = formatPrice(product.old_price)
-            product.price = formatPrice(product.price)
-
-            let files = await Product.files(product.id)
-            files = files.map(file => ({
-                ...file,
-                src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`,
-            }))
-
-            return res.render("products/show", { product, files })
+            return res.render("products/show", { product, files: product.files })
         } catch (error) {
             console.error(error)
         }
@@ -85,26 +70,15 @@ module.exports = {
     async edit (req, res){
 
         try {
-           
-            const product = await Product.find(req.params.id)
-
-            if(!product) return res.send("product not found!")
-        
-            product.old_price = formatPrice(product.old_price)
-            product.price = formatPrice(product.price)
-
+            const product = await LoadProductsService.load('product', {
+                where: {
+                    id: req.params.id
+                }
+            })
             //get categories
-            const categories = await Category.findAll
+            const categories = await Category.findAll()
             
-            //get images
-            let files = await Product.files(product.id)
-    
-            files = files.map(file => ({
-                ...file,
-                src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`,
-            }))
-
-            return res.render('products/edit', { product, categories, files }) 
+            return res.render('products/edit', { product, categories, files: product.files }) 
         } catch (error) {
             console.error(error)    
         }
@@ -144,7 +118,7 @@ module.exports = {
     
             if(req.body.old_price != req.body.price) {
                 const oldProduct = await Product.find(req.body.id)
-                req.body.old_price = oldProduct.rows[0].price
+                req.body.old_price = oldProduct.price
             }
     
             await Product.update(req.body.id, {
