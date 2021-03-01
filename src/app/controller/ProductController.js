0dd448e016138c2e009415ab59fa15127ffdac38
Vander Lima <vnd.vander@gmail.com>
2021-03-01 17:55:57 -0300

    Refactoring Validate Products with Validate Front

--- src/app/controller/ProductController.js
+++ src/app/controller/ProductController.js
@@ -17,17 +17,6 @@ module.exports = {
     },
     async post(req, res) {
         try {
-            const keys = Object.keys(req.body)
-
-            for(key of keys) {
-                if (req.body[key] == "") {
-                    return res.send('Please, fill all fields!')
-                }
-            }
-    
-            if (req.files.length == 0)
-                return res.send('Please, send at least one image')
-            
             let { category_id, name, description, old_price,
             price, quantity, status} = req.body
 
@@ -87,13 +76,7 @@ module.exports = {
     async put (req, res){
         
         try {
-            const keys = Object.keys(req.body)
-
-            for(key of keys) {
-                if (req.body[key] == "" && key != "removed_files") {
-                    return res.send('Please, fill')
-                }
-            }
+           
     
             if (req.files.length != 0) {
                const newFilesPromise = req.files.map(file =>
--- src/routes/products.js
+++ src/routes/products.js
@@ -6,6 +6,8 @@ const SearchController = require('../app/controller/SearchController')
 const ProductController = require('../app/controller/ProductController')
 
 const { onlyUsers } = require('../app/middlewares/session')
+
+const Validator = require('../app/validators/products')
 //SEARCH
 router.get('/search/products', SearchController.index)
 
@@ -14,8 +16,8 @@ router.get('/products/create', onlyUsers, ProductController.create)
 router.get('/products/:id', ProductController.show)
 router.get('/products/:id/edit', onlyUsers, ProductController.edit)
 
-router.post('/products', onlyUsers, multer.array("photo", 6),ProductController.post)
-router.put('/products', onlyUsers, multer.array("photo", 6),ProductController.put)
+router.post('/products', onlyUsers, multer.array("photo", 6), Validator.post, ProductController.post)
+router.put('/products', onlyUsers, multer.array("photo", 6), Validator.put, ProductController.put)
 router.delete('/products', onlyUsers, ProductController.delete)
 
 module.exports = router
