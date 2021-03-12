ca25b13bda48605f8fdd9a89e445b58b48153e18
Vander Lima <vnd.vander@gmail.com>
2021-03-12 12:04:48 -0300

    Cart Empty and Add Cart to Menu

--- src/app/controller/CartController.js
+++ src/app/controller/CartController.js
@@ -42,4 +42,14 @@ module.exports = {
 
         return res.redirect('/cart')
     },
+    delete(req, res) {
+        let { id } = req.params
+        let { cart } = req.session
+
+        if(!cart) return
+
+        req.session.cart = Cart.init(cart).delete(id)
+
+        return res.redirect('/cart')
+    }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -6,6 +6,7 @@ const CartController = require('../app/controller/CartController')
 router.get('/', CartController.index)
 router.post('/:id/add-one', CartController.addOne)
 router.post('/:id/remove-one', CartController.removeOne)
+router.post('/:id/delete', CartController.delete)
 
 
 module.exports = router
